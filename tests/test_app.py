f83e6abad4fb29b1d8ee4fef60eb55b05e89f6a0
Ayesha Ameen <Ayesha Ameen>
2023-11-24 01:10:55 +0500

    updated test_app/py file again

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,10 @@
 # tests/test_app.py
 import json
-from Devops_Prac.app import app  # Adjust the import path
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from app import app  # Adjust the import path
 
 def test_train_endpoint(client):
     # Test the /train endpoint
