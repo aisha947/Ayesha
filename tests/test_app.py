285525ac60c7068d6dc4a98ec7c927905d1dbb70
Ayesha Ameen <Ayesha Ameen>
2023-11-24 01:19:58 +0500

    updated app.py and test_app.py file to resolve the error

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,9 @@ import sys
 import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from Devops_Prac.app import app  # Adjust the import path
+
+from app import app
+  # Adjust the import path
 
 def test_train_endpoint(client):
     # Test the /train endpoint
