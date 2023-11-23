71c30a45b5900f895ac0dc1ecb14f57d8d964ea0
Ayesha Ameen <Ayesha Ameen>
2023-11-24 01:02:27 +0500

    Added tests folder

---
+++ tests/test_app.py
@@ -0,0 +1,28 @@
+# tests/test_app.py
+import json
+from app import app
+
+def test_train_endpoint(client):
+    # Test the /train endpoint
+    data = {'data': [20, 21, 22, 23, 24]}
+    response = client.post('/train', json=data)
+    assert response.status_code == 200
+    result = response.get_json()
+    assert 'message' in result
+    assert result['message'] == 'Model trained successfully'
+
+def test_predict_endpoint(client):
+    # Test the /api/predict endpoint
+    data = {'date': 16}  # Assuming you want to predict for the 16th day
+    response = client.post('/api/predict', json=data)
+    assert response.status_code == 200
+    result = response.get_json()
+    assert 'prediction' in result
+
+def test_invalid_data_predict_endpoint(client):
+    # Test the /api/predict endpoint with invalid data
+    data = {'invalid_key': 16}
+    response = client.post('/api/predict', json=data)
+    assert response.status_code == 500
+    result = response.get_json()
+    assert 'error' in result
